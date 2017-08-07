8c29bf5281d85d0f48d5e398d73799255f8d43e9
IndexTyl0rz <tyler32bit@gmail.com>
2017-08-06 21:29:00 -0700

    Changing schema verification keys

--- flask/helpfulDecorators.py
+++ flask/helpfulDecorators.py
@@ -7,14 +7,20 @@ from datetime import datetime
 
 def json_attribs_check(func):
     """
-        Decorator for validating json, a thing we might do often. 
+        Decorator for validating json, a thing we might do often.
+     
+        Use the standard syntctical sugar:
+            @json_attribs_check
+            def my_json_related_function():
+                doSomeStuff()
+        This syntax ensures @json_attribs_check always runs before defined func 
     """
     @wraps(func)
     def inner_func(jsonStr):
         gslvtsSchema = {"type":"object",
                         "properties":{
-                            "id": {"type":"number"}, 
-                            "utc": {"type":"string",
+                            "tagID": {"type":"number"}, 
+                            "UTC": {"type":"string",
                                     "format":"date-time"}
                             }
                        }
